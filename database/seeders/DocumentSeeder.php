ac437273876466f4f556e066ad6bdb59b0a1807f
safranatha <safranatha@gmail.com>
2025-12-03 13:17:48 +0700

    create seeder and factory for testing

---
+++ database/seeders/DocumentSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Proposal;
+use App\Models\SuratPenawaranHarga;
+use App\Models\Tender;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DocumentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Tender::factory(10)->create();
+        // SuratPenawaranHarga::factory(10)->create();
+        Proposal::factory(10)->create();
+    }
+}
