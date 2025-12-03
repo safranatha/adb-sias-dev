49045f9489422faeeef30b6f991335bbd03c5c33
safranatha <safranatha@gmail.com>
2025-12-03 21:02:55 +0700

    finalisasi view list proposal + button tender and button validate

--- app/Livewire/Proposal/Index.php
+++ app/Livewire/Proposal/Index.php
@@ -4,14 +4,18 @@ namespace App\Livewire\Proposal;
 
 use App\Models\Proposal;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'tailwind';
     public function render()
     {
         // dd(Proposal::all());
         return view('livewire.proposal.index', [
-            'proposals' => Proposal::all(),
+            'proposals' => Proposal::with(['tender', 'user'])->paginate(5),
         ]);
     }
 }
--- resources/views/livewire/proposal/index.blade.php
+++ resources/views/livewire/proposal/index.blade.php
@@ -1,4 +1,8 @@
 <div class="max-w-6xl mx-auto mt-8">
+    @can('create proposal')
+    <flux:button class="mb-4">Add Proposal</flux:button>
+    @endcan
+    
     <div class="overflow-x-auto rounded-xl border border-gray-200">
         <table class="w-full text-sm text-center">
             <thead class="bg-gray-100 text-black">
@@ -6,8 +10,11 @@
                     <th class="px-4 py-3 font-medium">Nama Tender</th>
                     <th class="px-4 py-3 font-medium">Nama Proposal</th>
                     <th class="px-4 py-3 font-medium">File Proposal</th>
+                    <th class="px-4 py-3 font-medium">Creator</th>
                     <th class="px-4 py-3 font-medium">Validator</th>
-                    {{-- <th class="px-4 py-3 font-medium">Action</th> --}}
+                    @can('validate proposal')
+                        <th class="px-4 py-3 font-medium">Action</th>
+                    @endcan
                 </tr>
             </thead>
 
@@ -28,12 +35,27 @@
                                 <flux:button icon="arrow-down-tray" class="mr-2"
                                     wire:click="download({{ $item->id }})"></flux:button>
                             </td>
-
+                            <td class="px-4 py-3">
+                                {{ $item->user->name }}
+                            </td>
+                            <td class="px-4 py-3">
+                                validator
+                            </td>
+                            @can('validate proposal')
+                                <td class="px-4 py-3">
+                                    <flux:button icon="check" class="mr-2" variant="primary" color="green">
+                                    </flux:button>
+                                    <flux:button icon="x-mark" variant="danger"></flux:button>
+                                </td>
+                            @endcan
                         </tr>
                     @endforeach
 
                 @endif
             </tbody>
         </table>
+        <div class=" pl-1 m-2">
+            {{ $proposals->links() }}
+        </div>
     </div>
 </div>
