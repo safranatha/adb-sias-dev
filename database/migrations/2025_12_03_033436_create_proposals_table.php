6d8739602cab07b3a382d2d9e21317cbdcc2b47e
safranatha <safranatha@gmail.com>
2025-12-03 13:16:36 +0700

    migrate document db

---
+++ database/migrations/2025_12_03_033436_create_proposals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tender_id')->unique()->constrained()->onDelete('cascade');
+            $table->string('nama_proposal');
+            $table->string('file_path_proposal');
+            // $table->string('nama_klien');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proposals');
+    }
+};
