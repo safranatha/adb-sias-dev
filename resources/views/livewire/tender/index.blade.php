a8a7f3c4f5986500fbe0349869e45d482f329e15
safranatha <safranatha@gmail.com>
2025-12-03 17:06:59 +0700

    make view tender

---
+++ resources/views/livewire/tender/index.blade.php
@@ -0,0 +1,33 @@
+<div>
+    <div class="max-w-6xl mx-auto mt-8">
+        <div class="overflow-x-auto rounded-xl border border-gray-200">
+            <table class="w-full text-sm text-center">
+                <thead class="bg-gray-100 text-black">
+                    <tr>
+                        <th class="px-4 py-3 font-medium">Nama Tender</th>
+                        <th class="px-4 py-3 font-medium">Nama Klien</th>
+                    </tr>
+                </thead>
+
+                <tbody class="divide-y divide-gray-200">
+                    @if ($tenders->isEmpty())
+                        <tr>
+                            <td colspan="3" class="px-4 py-6">
+                                Tidak ada tender untuk ditampilkan.
+                            </td>
+                        </tr>
+                    @else
+                        @foreach ($tenders as $item)
+                            <tr>
+                                <td class="px-4 py-3">{{ $item->nama_tender }}</td>
+                                <td class="px-4 py-3">{{ $item->nama_klien }}</td>
+                            </tr>
+                        @endforeach
+
+                    @endif
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
