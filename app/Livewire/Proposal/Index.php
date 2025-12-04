7fe902570f8a6da265d65c7c74bbf59d1c73e8a8
safranatha <safranatha@gmail.com>
2025-12-04 12:54:50 +0700

    add feature store/add proposal

--- app/Livewire/Proposal/Index.php
+++ app/Livewire/Proposal/Index.php
@@ -3,19 +3,60 @@
 namespace App\Livewire\Proposal;
 
 use App\Models\Proposal;
+use App\Models\Tender;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 use Livewire\WithPagination;
 
 class Index extends Component
 {
-    use WithPagination;
+    use WithPagination, WithFileUploads;
 
     protected $paginationTheme = 'tailwind';
+
+    public $tender_id;
+    public $nama_proposal;
+    public $file_path_proposal;
+
+
+    protected $rules = [
+        'tender_id' => ['required', 'exists:tenders,id'],
+        'nama_proposal' => ['required', 'string', 'max:255'],
+        'file_path_proposal' => ['required', 'file', 'mimes:pdf', 'max:10240'],
+    ];
+
+    public function store()
+    {
+        // dd($this->tender_id, $this->nama_proposal, $this->file_path_proposal);
+        $this->validate();
+
+        // save to laravel storage
+        $path = $this->file_path_proposal->store('proposals', 'public');
+
+        // save to database
+        Proposal::create([
+            'user_id' => auth()->user()->id,
+            'tender_id' => $this->tender_id,
+            'nama_proposal' => $this->nama_proposal,
+            'file_path_proposal' => $path,
+        ]);
+
+        $this->reset();
+
+        session()->flash('success', 'Proposal berhasil diupload!');
+
+        return redirect()->route('proposal.index');
+    }
+
+
     public function render()
     {
         // dd(Proposal::all());
         return view('livewire.proposal.index', [
             'proposals' => Proposal::with(['tender', 'user'])->paginate(5),
+            'tenders' => Tender::where('status', 'Dalam Proses')
+                ->doesntHave('proposal')
+                ->get(),
         ]);
     }
 }
--- resources/views/livewire/proposal/index.blade.php
+++ resources/views/livewire/proposal/index.blade.php
@@ -1,8 +1,62 @@
 <div class="max-w-6xl mx-auto mt-8">
     @can('create proposal')
-    <flux:button class="mb-4">Add Proposal</flux:button>
+        <flux:modal.trigger name="store-proposal">
+            <flux:button class="mb-4">Add Proposal</flux:button>
+        </flux:modal.trigger>
+
+        <flux:modal name="store-proposal" class="max-w-2xl">
+            <form wire:submit.prevent="store">
+                <flux:heading size="lg">Upload Proposal</flux:heading>
+                {{-- Name Field --}}
+                <flux:field>
+                    <flux:label class="mt-3">Nama Proposal</flux:label>
+                    <flux:input wire:model="nama_proposal" placeholder="Enter user name" />
+                </flux:field>
+
+
+                <flux:field>
+                    <flux:label class="mt-3">File Proposal</flux:label>
+                    <flux:input type="file" wire:model="file_path_proposal" />
+                    @error('file_path_proposal')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                    @enderror
+                </flux:field>
+
+                <flux:field>
+                    <flux:label class="mt-3">Tender</flux:label>
+                    <flux:select wire:model="tender_id">
+                        <flux:select.option value="">-- Pilih Tender --</flux:select.option>
+                        @foreach ($tenders as $tender)
+                            <flux:select.option value="{{ $tender->id }}">
+                                {{ $tender->nama_tender }}
+                            </flux:select.option>
+                        @endforeach
+                    </flux:select>
+
+                    @error('tender_id')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                    @enderror
+                </flux:field>
+
+                <flux:spacer />
+                <flux:button type="submit" class="mt-6" variant="primary">
+                    Upload
+                </flux:button>
+            </form>
+        </flux:modal>
     @endcan
-    
+
+    {{-- success message after post created --}}
+    @if (session('success'))
+        <div class="bg-green-100 text-green-800 px-4 py-2 rounded mb-3 successMsg">
+            {{ session('success') }}
+        </div>
+        <script>
+            setTimeout(() => {
+                document.getElementsByClassName('successMsg')?.remove();
+            }, 200);
+        </script>
+    @endif
     <div class="overflow-x-auto rounded-xl border border-gray-200">
         <table class="w-full text-sm text-center">
             <thead class="bg-gray-100 text-black">
