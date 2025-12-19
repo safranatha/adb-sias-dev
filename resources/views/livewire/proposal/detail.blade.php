e146317da229e89f57e144bcaaa2936e231000fb
safranatha <safranatha@gmail.com>
2025-12-19 15:57:43 +0700

    view detail proposal

--- resources/views/livewire/proposal/detail.blade.php
+++ resources/views/livewire/proposal/detail.blade.php
@@ -1,8 +1,44 @@
 <div>
     <flux:heading size="xl">Detail Proposal Tender</flux:heading>
-    <flux:text class="mt-2">Berikut merupakan detail Proposal Tender X.</flux:text>
+    <flux:text class="mt-2">Berikut merupakan detail Proposal Tender {{ $document_approvals->first()->name_tender }}.
+    </flux:text>
 
-    <div class="color-white bg-white p-10 rounded-lg shadow-md mt-6">
+    @foreach ($document_approvals as $item)
+        <div class="color-white bg-white p-10 rounded-lg shadow-md mt-6">
+            <flux:heading size="lg">Status</flux:heading>
+            <flux:text size="md" class="mt-2">{{ $item->status_proposal }}</flux:text>
+
+            <flux:heading size="lg" class="mt-6">Tanggal Revisi</flux:heading>
+            <flux:text size="md" class="mt-2">{{ $item->updated_at->format('d-m-y') }}</flux:text>
+
+            <flux:heading size="lg" class="mt-6">Tanggal Dibaca</flux:heading>
+            <flux:text size="md" class="mt-2">
+                {{ \Carbon\Carbon::parse($item->waktu_pesan_dibaca)->diffForHumans() }}</flux:text>
+
+
+
+            <flux:heading size="lg" class="mt-6">Status Dokumen</flux:heading>
+            @if ($item->status === 0)
+                <span class="bg-red-500 text-white text-s px-2 py-1 rounded-md">
+                    Rejected
+                </span>
+                <flux:heading size="lg" class="mt-6">Pesan Revisi Dokumen</flux:heading>
+                <flux:text size="md" class="mt-2">{{ $item->pesan_revisi }}</flux:text>
+            @else
+                <span class="bg-green-500 text-white text-s px-2 py-1 rounded-md">
+                    Approved
+                </span>
+            @endif
+
+
+            {{-- <flux:heading size="lg" class="mt-6">Dokumen Revisi</flux:heading>
+            <flux:button href="#" variant="primary" color="emerald" class="mt-2" icon="arrow-down-tray">Unduh
+                Dokumen Revisi</flux:button> --}}
+        </div>
+    @endforeach
+
+
+    {{-- <div class="color-white bg-white p-10 rounded-lg shadow-md mt-6">
         <flux:heading size="lg">Status</flux:heading>
         <flux:text size="md" class="mt-2">Proposal Baru</flux:text>
 
@@ -17,23 +53,6 @@
         
         <flux:heading size="lg" class="mt-6">Dokumen Revisi</flux:heading>
         <flux:button href="#" variant="primary" color="emerald" class="mt-2" icon="arrow-down-tray">Unduh Dokumen Revisi</flux:button>
-    </div>
-
-    <div class="color-white bg-white p-10 rounded-lg shadow-md mt-6">
-        <flux:heading size="lg">Status</flux:heading>
-        <flux:text size="md" class="mt-2">Proposal Baru</flux:text>
-
-        <flux:heading size="lg" class="mt-6">Tanggal Revisi</flux:heading>
-        <flux:text size="md" class="mt-2">dd/mm/yyyy</flux:text>
-
-        <flux:heading size="lg" class="mt-6">Tanggal Dibaca</flux:heading>
-        <flux:text size="md" class="mt-2">dd/mm/yyyy</flux:text>
-        
-        <flux:heading size="lg" class="mt-6">Pesan Revisi</flux:heading>
-        <flux:text size="md" class="mt-2">Lorem ipsum sit dolor amet..</flux:text>
-        
-        <flux:heading size="lg" class="mt-6">Dokumen Revisi</flux:heading>
-        <flux:button href="#" variant="primary" color="emerald" class="mt-2" icon="arrow-down-tray">Unduh Dokumen Revisi</flux:button>
-    </div>
+    </div> --}}
 
 </div>
