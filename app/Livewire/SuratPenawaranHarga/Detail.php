074f00f8a82922e646e88831b98b71522ec763a8
safranatha <safranatha@gmail.com>
2025-12-19 16:19:06 +0700

    add feat logic detail page

--- app/Livewire/SuratPenawaranHarga/Detail.php
+++ app/Livewire/SuratPenawaranHarga/Detail.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire\SuratPenawaranHarga;
 
+use App\Models\DocumentApprovalWorkflow;
 use Livewire\Component;
 
 class Detail extends Component
 {
+
+    public $surat_penawaran_harga_id;
+
+    public function mount($id)
+    {
+        $this->surat_penawaran_harga_id = $id;
+    }
+
     public function render()
     {
-        return view('livewire.surat-penawaran-harga.detail');
+        return view('livewire.surat-penawaran-harga.detail', [
+            'document_approvals' => DocumentApprovalWorkflow::with(['surat_penawaran_harga'])
+                ->where('surat_penawaran_harga_id', $this->surat_penawaran_harga_id)
+                ->orderByDesc('created_at')
+                ->get(),
+        ]);
     }
 }
