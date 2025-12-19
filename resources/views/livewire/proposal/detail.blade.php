de8e4eda1383ee90a1f04e99b58d3b5ddae2b463
safranatha <safranatha@gmail.com>
2025-12-19 16:17:32 +0700

    penyesuaian nama helper (tak ganti)

--- resources/views/livewire/proposal/detail.blade.php
+++ resources/views/livewire/proposal/detail.blade.php
@@ -1,6 +1,6 @@
 <div>
     <flux:heading size="xl">Detail Proposal Tender</flux:heading>
-    <flux:text class="mt-2">Berikut merupakan detail Proposal Tender {{ $document_approvals->first()->name_tender }}.
+    <flux:text class="mt-2">Berikut merupakan detail Proposal Tender {{ $document_approvals->first()->name_tender_proposal }}.
     </flux:text>
 
     @foreach ($document_approvals as $item)
@@ -36,23 +36,5 @@
                 Dokumen Revisi</flux:button> --}}
         </div>
     @endforeach
-
-
-    {{-- <div class="color-white bg-white p-10 rounded-lg shadow-md mt-6">
-        <flux:heading size="lg">Status</flux:heading>
-        <flux:text size="md" class="mt-2">Proposal Baru</flux:text>
-
-        <flux:heading size="lg" class="mt-6">Tanggal Revisi</flux:heading>
-        <flux:text size="md" class="mt-2">dd/mm/yyyy</flux:text>
-
-        <flux:heading size="lg" class="mt-6">Tanggal Dibaca</flux:heading>
-        <flux:text size="md" class="mt-2">dd/mm/yyyy</flux:text>
-        
-        <flux:heading size="lg" class="mt-6">Pesan Revisi</flux:heading>
-        <flux:text size="md" class="mt-2">Lorem ipsum sit dolor amet..</flux:text>
-        
-        <flux:heading size="lg" class="mt-6">Dokumen Revisi</flux:heading>
-        <flux:button href="#" variant="primary" color="emerald" class="mt-2" icon="arrow-down-tray">Unduh Dokumen Revisi</flux:button>
-    </div> --}}
 
 </div>
