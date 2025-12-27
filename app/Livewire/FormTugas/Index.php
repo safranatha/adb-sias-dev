da4f9e548bcce7cceb8c1ddf410ca9e1c83bceec
safranatha <safranatha@gmail.com>
2025-12-27 16:33:49 +0700

    hapus function yang tidak digunakan

--- app/Livewire/FormTugas/Index.php
+++ app/Livewire/FormTugas/Index.php
@@ -13,22 +13,6 @@ class Index extends Component
     protected $paginationTheme = 'tailwind';
 
 
-    public function download($id)
-    {
-        $form_tugas = FormTugas::findOrFail($id);
-
-        if (!$form_tugas) {
-            return session()->flash('error', 'Form Tugas tidak ditemukan.');
-        }
-
-        $file_path = public_path('storage/' . $form_tugas->file_path_form_tugas);
-
-        if (!file_exists($file_path)) {
-            return session()->flash('error', 'File Form Tugas tidak ditemukan di storage.');
-        }
-
-        return response()->download($file_path);
-    }
     public function render()
     {
         return view('livewire.form-tugas.index', [
