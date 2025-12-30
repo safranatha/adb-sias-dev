7beb737ea84a6c10cbd1201d48f49ae785cb2d27
safranatha <safranatha@gmail.com>
2025-12-30 18:43:55 +0700

    add helper for reject and approve dokumen sph

--- app/Helpers/ApprovalTenderDocHelper.php
+++ app/Helpers/ApprovalTenderDocHelper.php
@@ -55,4 +55,47 @@ class ApprovalTenderDocHelper
 
         return $documentApproval;
     }
+
+    public static function rejectDocumentSPH(
+        string $modelClass,
+        int $id,
+        string $nama_role,
+        string $pesan_revisi,
+        string $path
+    ) {
+        // Cari document approval berdasarkan surat_penawaran_harga_id
+        $documentApproval = $modelClass::where('surat_penawaran_harga_id', $id)
+            ->latest() // Ambil yang terbaru
+            ->first();
+
+        $documentApproval->update([
+            'user_id' => auth()->user()->id,
+            'status' => false,
+            'level' => ($nama_role == "Manajer Admin") ? "2" : ($nama_role == "Direktur" ? "3" : null),
+            'file_path_revisi' => $path,
+            'pesan_revisi' => $pesan_revisi,
+            'keterangan' => ($nama_role == "Manajer Admin") ? "Surat Penawaran Harga ditolak oleh Manajer Admin" : ($nama_role == "Direktur" ? "Surat Penawaran Harga ditolak oleh Direktur" : null),
+        ]);
+        return $documentApproval;
+    }
+
+    public static function approveDocumentSPH(
+        string $modelClass,
+        int $id,
+        string $nama_role
+    ) {
+         // Cari document approval berdasarkan surat_penawaran_harga_id
+        $documentApproval = $modelClass::where('surat_penawaran_harga_id', $id)
+            ->latest() // Ambil yang terbaru
+            ->first();
+
+        $documentApproval->update([
+            'user_id' => auth()->user()->id,
+            'status' => true,
+            'level' => ($nama_role == "Manajer Admin") ? "2" : ($nama_role == "Direktur" ? "3" : null),
+            'keterangan' => ($nama_role == "Manajer Admin") ? "Surat Penawaran Harga disetujui oleh Manajer Admin" : ($nama_role == "Direktur" ? "Surat Penawaran Harga disetujui oleh Direktur" : null),
+        ]);
+
+        return $documentApproval;
+    }
 }
