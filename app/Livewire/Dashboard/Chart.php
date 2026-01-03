697872b25f9041648c74c74cdb7bc2ea482d65a4
safranatha <safranatha@gmail.com>
2026-01-03 18:22:04 +0700

    add feat chart tender

--- app/Livewire/Dashboard/Chart.php
+++ app/Livewire/Dashboard/Chart.php
@@ -5,15 +5,19 @@
 
 namespace App\Livewire\Dashboard;
 
+use App\Models\Tender;
 use Livewire\Component;
 
 class Chart extends Component
 {
     public function render()
     {
+        $gagal=Tender::where('status','=','Gagal')->count();
+        $berhasil=Tender::where('status','=','Berhasil')->count();
+        $dalam_proses=Tender::where('status','=','Dalam Proses')->count();
         return view('livewire.dashboard.chart', [
-            'labels' => ['Belum Berjalan','Berjalan','Selesai'],
-            'values' => [10,7,20],
+            'labels' => ['Gagal','Berhasil','Dalam Proses'],
+            'values' => [$gagal,$berhasil,$dalam_proses],
         ]);
     }
 }
