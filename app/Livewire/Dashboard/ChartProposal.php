f4833d3698b9f14ebb862731f9184f25581c6df5
safranatha <safranatha@gmail.com>
2026-01-03 19:35:02 +0700

    implement function

--- app/Livewire/Dashboard/ChartProposal.php
+++ app/Livewire/Dashboard/ChartProposal.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Dashboard;
 
+use App\Helpers\DokumenChart;
 use App\Models\DocumentApprovalWorkflow;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
@@ -10,51 +11,17 @@ class ChartProposal extends Component
 {
     public function render()
     {
-        $proposal_belum_disetujui_manajer = DocumentApprovalWorkflow::whereNull('status')->where('level', '=', 0)->whereNotNull('proposal_id')->count();
+        $proposal_belum_disetujui_manajer = DokumenChart::dokumenBelumDisetujuiManajer('proposal_id');
 
         $proposal_ditolak_manajer =
-            DocumentApprovalWorkflow::where('level', 2)
-                ->where('status', 0)
-                ->whereNotNull('proposal_id')
-                ->whereNotExists(function ($query) {
-                    $query->select(DB::raw(1))
-                        ->from('document_approval_workflow as daw2')
-                        ->whereColumn('daw2.proposal_id', 'document_approval_workflow.proposal_id')
-                        ->whereColumn('daw2.id', '>', 'document_approval_workflow.id');
-                })
-                ->count();
+            DokumenChart::dokumenDitolakManajer('proposal_id');
 
-        $proposal_sudah_disetujui_manajer =
-            DocumentApprovalWorkflow::where('level', 2)
-                ->where('status', 1)
-                ->whereNotNull('proposal_id')
-                ->whereNotExists(function ($query) {
-                    $query->select(DB::raw(1))
-                        ->from('document_approval_workflow as daw2')
-                        ->whereColumn('daw2.proposal_id', 'document_approval_workflow.proposal_id')
-                        ->whereColumn('daw2.id', '>', 'document_approval_workflow.id')
-                        ->where('daw2.level', 3);
-                    // ->where('daw2.status', 0);
-                })
-                ->count('proposal_id');
+        $proposal_sudah_disetujui_manajer = DokumenChart::dokumenSudahDisetujuiManajer('proposal_id');
 
-        $proposal_ditolak_direktur =
-            DocumentApprovalWorkflow::where('level', 3)
-                ->where('status', 0)
-                ->whereNotNull('proposal_id')
-                ->whereNotExists(function ($query) {
-                    $query->select(DB::raw(1))
-                        ->from('document_approval_workflow as daw2')
-                        ->whereColumn('daw2.proposal_id', 'document_approval_workflow.proposal_id')
-                        ->whereColumn('daw2.id', '>', 'document_approval_workflow.id');
-                })
-                ->count();
+        $proposal_ditolak_direktur = DokumenChart::dokumenDitolakDirektur('proposal_id');
 
-        $proposal_sudah_disetujui_direktur =
-            DocumentApprovalWorkflow::where('level', 3)
-                ->where('status', 1)
-                ->whereNotNull('proposal_id')
-                ->count('proposal_id');
+        $proposal_sudah_disetujui_direktur = DokumenChart::dokumenSudahDisetujuiDirektur('proposal_id');
+            
 
         return view('livewire.dashboard.chart-proposal', [
             'labels' => ['Belum disetujui Manajer', 'Tolak manajer', 'Belum disetujui Direktur', 'Tolak direktur', 'Disetujui direktur'],
--- app/Livewire/Dashboard/ChartSph.php
+++ app/Livewire/Dashboard/ChartSph.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Dashboard;
 
+use App\Helpers\DokumenChart;
 use App\Models\DocumentApprovalWorkflow;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
@@ -10,51 +11,17 @@ class ChartSph extends Component
 {
     public function render()
     {
-        $sph_belum_disetujui_manajer = DocumentApprovalWorkflow::whereNull('status')->where('level', '=', 0)->whereNotNull('surat_penawaran_harga_id')->count();
+        $sph_belum_disetujui_manajer = DokumenChart::dokumenBelumDisetujuiManajer('surat_penawaran_harga_id');
 
         $sph_ditolak_manajer =
-            DocumentApprovalWorkflow::where('level', 2)
-                ->where('status', 0)
-                ->whereNotNull('surat_penawaran_harga_id')
-                ->whereNotExists(function ($query) {
-                    $query->select(DB::raw(1))
-                        ->from('document_approval_workflow as daw2')
-                        ->whereColumn('daw2.surat_penawaran_harga_id', 'document_approval_workflow.surat_penawaran_harga_id')
-                        ->whereColumn('daw2.id', '>', 'document_approval_workflow.id');
-                })
-                ->count();
+            DokumenChart::dokumenDitolakManajer('surat_penawaran_harga_id');
 
         $sph_sudah_disetujui_manajer =
-            DocumentApprovalWorkflow::where('level', 2)
-                ->where('status', 1)
-                ->whereNotNull('surat_penawaran_harga_id')
-                ->whereNotExists(function ($query) {
-                    $query->select(DB::raw(1))
-                        ->from('document_approval_workflow as daw2')
-                        ->whereColumn('daw2.surat_penawaran_harga_id', 'document_approval_workflow.surat_penawaran_harga_id')
-                        ->whereColumn('daw2.id', '>', 'document_approval_workflow.id')
-                        ->where('daw2.level', 3);
-                    // ->where('daw2.status', 0);
-                })
-                ->count('surat_penawaran_harga_id');
+            DokumenChart::dokumenSudahDisetujuiManajer('surat_penawaran_harga_id');
 
-        $sph_ditolak_direktur =
-            DocumentApprovalWorkflow::where('level', 3)
-                ->where('status', 0)
-                ->whereNotNull('surat_penawaran_harga_id')
-                ->whereNotExists(function ($query) {
-                    $query->select(DB::raw(1))
-                        ->from('document_approval_workflow as daw2')
-                        ->whereColumn('daw2.surat_penawaran_harga_id', 'document_approval_workflow.surat_penawaran_harga_id')
-                        ->whereColumn('daw2.id', '>', 'document_approval_workflow.id');
-                })
-                ->count();
+        $sph_ditolak_direktur = DokumenChart::dokumenDitolakDirektur('surat_penawaran_harga_id');
 
-        $sph_sudah_disetujui_direktur =
-            DocumentApprovalWorkflow::where('level', 3)
-                ->where('status', 1)
-                ->whereNotNull('surat_penawaran_harga_id')
-                ->count('surat_penawaran_harga_id');
+        $sph_sudah_disetujui_direktur = DokumenChart::dokumenSudahDisetujuiDirektur('surat_penawaran_harga_id');
 
         return view('livewire.dashboard.chart-sph', [
             'labels' => ['Belum disetujui Manajer', 'Tolak manajer', 'Belum disetujui Direktur', 'Tolak direktur', 'Disetujui direktur'],
