a7510599d11ab6af49288e8ceddeaf97595926cd
safranatha <safranatha@gmail.com>
2026-01-03 18:09:08 +0700

    add feat proposal chart

--- app/Livewire/Dashboard/ChartProposal.php
+++ app/Livewire/Dashboard/ChartProposal.php
@@ -12,6 +12,18 @@ class ChartProposal extends Component
     {
         $proposal_belum_disetujui_manajer = DocumentApprovalWorkflow::whereNull('status')->where('level', '=', 0)->whereNotNull('proposal_id')->count();
 
+        $proposal_ditolak_manajer =
+            DocumentApprovalWorkflow::where('level', 2)
+                ->where('status', 0)
+                ->whereNotNull('proposal_id')
+                ->whereNotExists(function ($query) {
+                    $query->select(DB::raw(1))
+                        ->from('document_approval_workflow as daw2')
+                        ->whereColumn('daw2.proposal_id', 'document_approval_workflow.proposal_id')
+                        ->whereColumn('daw2.id', '>', 'document_approval_workflow.id');
+                })
+                ->count();
+
         $proposal_sudah_disetujui_manajer =
             DocumentApprovalWorkflow::where('level', 2)
                 ->where('status', 1)
@@ -30,24 +42,23 @@ class ChartProposal extends Component
             DocumentApprovalWorkflow::where('level', 3)
                 ->where('status', 0)
                 ->whereNotNull('proposal_id')
-                ->whereExists(function ($query) {
+                ->whereNotExists(function ($query) {
                     $query->select(DB::raw(1))
                         ->from('document_approval_workflow as daw2')
                         ->whereColumn('daw2.proposal_id', 'document_approval_workflow.proposal_id')
                         ->whereColumn('daw2.id', '>', 'document_approval_workflow.id');
                 })
-                ->distinct('proposal_id')
                 ->count();
-        
-        $proposal_sudah_disetujui_direktur=
+
+        $proposal_sudah_disetujui_direktur =
             DocumentApprovalWorkflow::where('level', 3)
                 ->where('status', 1)
                 ->whereNotNull('proposal_id')
                 ->count('proposal_id');
 
         return view('livewire.dashboard.chart-proposal', [
-            'labels' => ['Belum disetujui Manajer', 'Belum disetujui Direktur', 'TOlak direktur', 'Disetujui direktur'],
-            'values' => [$proposal_belum_disetujui_manajer, $proposal_sudah_disetujui_manajer, $proposal_ditolak_direktur, $proposal_sudah_disetujui_direktur],
+            'labels' => ['Belum disetujui Manajer', 'Tolak manajer', 'Belum disetujui Direktur', 'Tolak direktur', 'Disetujui direktur'],
+            'values' => [$proposal_belum_disetujui_manajer, $proposal_ditolak_manajer, $proposal_sudah_disetujui_manajer, $proposal_ditolak_direktur, $proposal_sudah_disetujui_direktur],
         ]);
     }
 }
--- resources/views/livewire/dashboard/chart-proposal.blade.php
+++ resources/views/livewire/dashboard/chart-proposal.blade.php
@@ -1,56 +1,57 @@
 <div class="">
     <flux:heading size="lg" class="bg-green-50 p-3 text-accent-content text-center">Rekap Proposal</flux:heading>
     <div class="flex items-center justify-center">
-    <div class="w-120 h-120 mt-5 mb-5">
-        <canvas id="proposalChart"></canvas>
+        <div class="w-120 h-120 mt-5 mb-5">
+            <canvas id="proposalChart"></canvas>
+        </div>
     </div>
-</div>
 
 </div>
 
 @push('scripts')
-<script>
-    document.addEventListener('livewire:navigated', initChart);
-    document.addEventListener('DOMContentLoaded', initChart);
+    <script>
+        document.addEventListener('livewire:navigated', initChart);
+        document.addEventListener('DOMContentLoaded', initChart);
 
-    function initChart() {
-        const ctx = document.getElementById('proposalChart');
-        if (!ctx) return;
+        function initChart() {
+            const ctx = document.getElementById('proposalChart');
+            if (!ctx) return;
 
-        // Hindari double render
-        if (ctx.chart) {
-            ctx.chart.destroy();
-        }
-
-        ctx.chart = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-                labels: @json($labels),
-                datasets: [{
-                    label: 'Jumlah',
-                    data: @json($values),
-                    backgroundColor: [
-                        '#B31919',
-                        '#C97E04',
-                        '#050A55',
-                        '#057A55',         
-                    ],
-                    borderWidth: 2
-                }]
-            },
-            options: {
-                plugins: {
-                    
-                    legend: {
-                        display:true,
-                        position:'right',
-                    }
-                },
-                maintainAspectRatio: true,
-                aspectRatio:2,
-                responsive: true,
+            // Hindari double render
+            if (ctx.chart) {
+                ctx.chart.destroy();
             }
-        });
-    }
-</script>
+
+            ctx.chart = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    labels: @json($labels),
+                    datasets: [{
+                        label: 'Jumlah',
+                        data: @json($values),
+                        backgroundColor: [
+                            '#B31919',
+                            '#FFF44F',
+                            '#C97E04',
+                            '#050A55',
+                            '#057A55',
+                        ],
+                        borderWidth: 2
+                    }]
+                },
+                options: {
+                    plugins: {
+
+                        legend: {
+                            display: true,
+                            position: 'right',
+                        }
+                    },
+                    maintainAspectRatio: true,
+                    aspectRatio: 2,
+                    responsive: true,
+                }
+            });
+        }
+    </script>
 @endpush
