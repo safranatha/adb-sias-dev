ddd60eb5747143a05425781e58b29c6d7d545438
faishalfhid <faishalfh1.work@gmail.com>
2026-01-05 00:23:45 +0700

    Add style to search tracking document

--- resources/views/livewire/tracking-document/index.blade.php
+++ resources/views/livewire/tracking-document/index.blade.php
@@ -1,3 +1,178 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
-</div>
+    <flux:heading size="xl">Tracking Document</flux:heading>
+    <flux:text class="mt-2">Cari dokumen tender Adi Banuwa</flux:text>
+
+    <flux:separator class="mt-5 mb-5" />
+    <div class="flex gap-4">
+        <flux:button.group class="w-full flex">
+            <div class="flex-[3]">
+                <flux:input icon="magnifying-glass" wire:model="search" placeholder="Temukan Tender..." />
+            </div>
+
+            <div class="flex-[1]">
+                <flux:select size="md" placeholder="Cari berdasarkan...">
+                    <flux:select.option>ID Tender</flux:select.option>
+                    <flux:select.option>Nama Tender</flux:select.option>
+                    <flux:select.option>Perusahaan Tender</flux:select.option>
+                </flux:select>
+            </div>
+        </flux:button.group>
+
+        <flux:button variant="primary" class="w-32" color="emerald">
+            Cari
+        </flux:button>
+    </div>
+
+    <flux:separator class="mt-5 mb-5" />
+    <div class="bg-white p-5 rounded-xl outline-2 outline-green-50 mt-5">
+        <div class="flex justify-between mx-auto">
+            <div class="flex gap-4 items-center">
+                <flux:icon.document-text class="size-25" />
+                <div>
+                    <flux:heading size="lg">Nama Tender</flux:heading>
+                    <flux:text class="mt-2">PT Lorem Ipsum</flux:text>
+                    <div class="mt-3 flex gap-5">
+                        <div>
+                            <flux:text>Tanggal Dibuat</flux:text>
+                            <flux:badge variant="outline" color="green" class="mt-1">dd/mm/yyyy</flux:badge>
+                        </div>
+                        <div>
+                            <flux:text>Status Tender</flux:text>
+                            <flux:badge variant="outline" color="gray" class="mt-1">Lorem Ipsum</flux:badge>
+                        </div>
+                        <div>
+                            <flux:text>ID Tender</flux:text>
+                            <flux:badge variant="outline" color="blue" class="mt-1">xxx</flux:badge>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center">
+                <flux:button icon="magnifying-glass" variant="primary" color="emerald">
+                    Detail Tender
+                </flux:button>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-white p-5 rounded-xl outline-2 outline-green-50 mt-5">
+        <div class="flex justify-between mx-auto">
+            <div class="flex gap-4 items-center">
+                <flux:icon.document-text class="size-25" />
+                <div>
+                    <flux:heading size="lg">Nama Tender</flux:heading>
+                    <flux:text class="mt-2">PT Lorem Ipsum</flux:text>
+                    <div class="mt-3 flex gap-5">
+                        <div>
+                            <flux:text>Tanggal Dibuat</flux:text>
+                            <flux:badge variant="outline" color="green" class="mt-1">dd/mm/yyyy</flux:badge>
+                        </div>
+                        <div>
+                            <flux:text>Status Tender</flux:text>
+                            <flux:badge variant="outline" color="gray" class="mt-1">Lorem Ipsum</flux:badge>
+                        </div>
+                        <div>
+                            <flux:text>ID Tender</flux:text>
+                            <flux:badge variant="outline" color="blue" class="mt-1">xxx</flux:badge>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center">
+                <flux:button icon="magnifying-glass" variant="primary" color="emerald">
+                    Detail Tender
+                </flux:button>
+            </div>
+        </div>
+    </div>
+    <div class="bg-white p-5 rounded-xl outline-2 outline-green-50 mt-5">
+        <div class="flex justify-between mx-auto">
+            <div class="flex gap-4 items-center">
+                <flux:icon.document-text class="size-25" />
+                <div>
+                    <flux:heading size="lg">Nama Tender</flux:heading>
+                    <flux:text class="mt-2">PT Lorem Ipsum</flux:text>
+                    <div class="mt-3 flex gap-5">
+                        <div>
+                            <flux:text>Tanggal Dibuat</flux:text>
+                            <flux:badge variant="outline" color="green" class="mt-1">dd/mm/yyyy</flux:badge>
+                        </div>
+                        <div>
+                            <flux:text>Status Tender</flux:text>
+                            <flux:badge variant="outline" color="gray" class="mt-1">Lorem Ipsum</flux:badge>
+                        </div>
+                        <div>
+                            <flux:text>ID Tender</flux:text>
+                            <flux:badge variant="outline" color="blue" class="mt-1">xxx</flux:badge>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center">
+                <flux:button icon="magnifying-glass" variant="primary" color="emerald">
+                    Detail Tender
+                </flux:button>
+            </div>
+        </div>
+    </div>
+    <div class="bg-white p-5 rounded-xl outline-2 outline-green-50 mt-5">
+        <div class="flex justify-between mx-auto">
+            <div class="flex gap-4 items-center">
+                <flux:icon.document-text class="size-25" />
+                <div>
+                    <flux:heading size="lg">Nama Tender</flux:heading>
+                    <flux:text class="mt-2">PT Lorem Ipsum</flux:text>
+                    <div class="mt-3 flex gap-5">
+                        <div>
+                            <flux:text>Tanggal Dibuat</flux:text>
+                            <flux:badge variant="outline" color="green" class="mt-1">dd/mm/yyyy</flux:badge>
+                        </div>
+                        <div>
+                            <flux:text>Status Tender</flux:text>
+                            <flux:badge variant="outline" color="gray" class="mt-1">Lorem Ipsum</flux:badge>
+                        </div>
+                        <div>
+                            <flux:text>ID Tender</flux:text>
+                            <flux:badge variant="outline" color="blue" class="mt-1">xxx</flux:badge>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center">
+                <flux:button icon="magnifying-glass" variant="primary" color="emerald">
+                    Detail Tender
+                </flux:button>
+            </div>
+        </div>
+    </div>
+    <div class="bg-white p-5 rounded-xl outline-2 outline-green-50 mt-5">
+        <div class="flex justify-between mx-auto">
+            <div class="flex gap-4 items-center">
+                <flux:icon.document-text class="size-25" />
+                <div>
+                    <flux:heading size="lg">Nama Tender</flux:heading>
+                    <flux:text class="mt-2">PT Lorem Ipsum</flux:text>
+                    <div class="mt-3 flex gap-5">
+                        <div>
+                            <flux:text>Tanggal Dibuat</flux:text>
+                            <flux:badge variant="outline" color="green" class="mt-1">dd/mm/yyyy</flux:badge>
+                        </div>
+                        <div>
+                            <flux:text>Status Tender</flux:text>
+                            <flux:badge variant="outline" color="gray" class="mt-1">Lorem Ipsum</flux:badge>
+                        </div>
+                        <div>
+                            <flux:text>ID Tender</flux:text>
+                            <flux:badge variant="outline" color="blue" class="mt-1">xxx</flux:badge>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="flex items-center">
+                <flux:button icon="magnifying-glass" variant="primary" color="emerald">
+                    Detail Tender
+                </flux:button>
+            </div>
+        </div>
+    </div>
