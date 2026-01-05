df80c6820ae94e239af0856cb287fdd929b8a309
safranatha <safranatha@gmail.com>
2026-01-05 12:23:27 +0700

    migrate busines logic chart sph from helper to services

--- app/Livewire/Dashboard/ChartProposal.php
+++ app/Livewire/Dashboard/ChartProposal.php
@@ -2,30 +2,52 @@
 
 namespace App\Livewire\Dashboard;
 
-use App\Helpers\DokumenChart;
+use App\Services\Tender\DokumenChartService;
 use App\Models\DocumentApprovalWorkflow;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
 
 class ChartProposal extends Component
 {
+    protected DokumenChartService $dokumenChartService;
+
+    public function boot(DokumenChartService $dokumenChartService)
+    {
+        $this->dokumenChartService = $dokumenChartService;
+    }
+
     public function render()
     {
-        $proposal_belum_disetujui_manajer = DokumenChart::dokumenBelumDisetujuiManajer('proposal_id');
+        $proposal_belum_disetujui_manajer =
+            $this->dokumenChartService->dokumenBelumDisetujuiManajer('proposal_id');
 
         $proposal_ditolak_manajer =
-            DokumenChart::dokumenDitolakManajer('proposal_id');
+            $this->dokumenChartService->dokumenDitolakManajer('proposal_id');
 
-        $proposal_sudah_disetujui_manajer = DokumenChart::dokumenSudahDisetujuiManajer('proposal_id');
+        $proposal_sudah_disetujui_manajer =
+            $this->dokumenChartService->dokumenSudahDisetujuiManajer('proposal_id');
 
-        $proposal_ditolak_direktur = DokumenChart::dokumenDitolakDirektur('proposal_id');
+        $proposal_ditolak_direktur =
+            $this->dokumenChartService->dokumenDitolakDirektur('proposal_id');
 
-        $proposal_sudah_disetujui_direktur = DokumenChart::dokumenSudahDisetujuiDirektur('proposal_id');
-            
+        $proposal_sudah_disetujui_direktur =
+            $this->dokumenChartService->dokumenSudahDisetujuiDirektur('proposal_id');
 
         return view('livewire.dashboard.chart-proposal', [
-            'labels' => ['Belum disetujui Manajer', 'Tolak manajer', 'Belum disetujui Direktur', 'Tolak direktur', 'Disetujui direktur'],
-            'values' => [$proposal_belum_disetujui_manajer, $proposal_ditolak_manajer, $proposal_sudah_disetujui_manajer, $proposal_ditolak_direktur, $proposal_sudah_disetujui_direktur],
+            'labels' => [
+                'Belum disetujui Manajer',
+                'Tolak manajer',
+                'Belum disetujui Direktur',
+                'Tolak direktur',
+                'Disetujui direktur'
+            ],
+            'values' => [
+                $proposal_belum_disetujui_manajer,
+                $proposal_ditolak_manajer,
+                $proposal_sudah_disetujui_manajer,
+                $proposal_ditolak_direktur,
+                $proposal_sudah_disetujui_direktur
+            ],
         ]);
     }
 }
