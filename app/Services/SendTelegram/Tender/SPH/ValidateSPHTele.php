927dec4635942268cce188a8565a259d51a6ac57
safranatha <safranatha@gmail.com>
2026-01-28 20:11:26 +0700

    add service for use on api tele

---
+++ app/Services/SendTelegram/Tender/SPH/ValidateSPHTele.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Services\SendTelegram\Tender\SPH;
+
+use App\Models\User;
+use Http;
+
+class ValidateSPHTele
+{
+    protected string $token;
+
+    public function __construct()
+    {
+        $this->token = config('services.telegram.bot_token');
+    }
+
+    public function sendMessageToManajer(string $text): void
+    {
+        $manager = User::role('Manajer Admin')
+            ->whereNotNull('telegram_chat_id')
+            ->first();
+
+        $chatId = $manager->telegram_chat_id;
+
+        Http::post("https://api.telegram.org/bot{$this->token}/sendMessage", [
+            'chat_id' => $chatId,
+            'text' => $text,
+        ]);
+    }
+
+    public function sendMessageToDirektur(string $text): void
+    {
+        $direktur = User::role('Direktur')
+            ->whereNotNull('telegram_chat_id')
+            ->first();
+
+        $chatId = $direktur->telegram_chat_id;
+
+        Http::post("https://api.telegram.org/bot{$this->token}/sendMessage", [
+            'chat_id' => $chatId,
+            'text' => $text,
+        ]);
+    }
+}
